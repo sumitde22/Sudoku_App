2caa9ba3dac0c925d86e39b7ed179a03f43a014b
Dev <dev@Sumits-MBP.T-mobile.com>
2021-04-13 18:15:17 -0400

    disable debug for prod

--- sudoku_project/settings.py
+++ sudoku_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*@3$(fow@@k6i4rejiveck*kzo3luf=hi8pu!n09i6d!&_7&_t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
