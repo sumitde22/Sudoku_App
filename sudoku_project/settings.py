2880892f08363cd2305bd53eba77823839f69136
Dev <dev@Sumits-MBP.T-mobile.com>
2021-04-13 15:37:31 -0400

    added some configs for deployment

--- sudoku_project/settings.py
+++ sudoku_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,5 +126,8 @@ STATIC_URL = '/static/'
 # Django authentication configuration settings
 
 LOGOUT_REDIRECT_URL='/accounts/login'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 
